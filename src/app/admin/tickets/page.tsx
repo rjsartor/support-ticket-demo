981e85820c9b092fcd9decc068a0d2de061d8ce5
RJ <randal.sartor@gmail.com>
2024-03-14 20:41:42 -0400

    feat: updates pages and layouts

--- src/app/admin/tickets/page.tsx
+++ src/app/admin/tickets/page.tsx
@@ -1,12 +1,40 @@
-'use client';
-import { FC } from 'react';
+import { SupportTicketStatusEnum } from '@/app/lib/definitions';
+import { Suspense } from 'react';
+import DataList from '@/app/components/data-list';
+import TicketRow from '@/app/components/ticket-row';
+import { TICKET_OFFSET, fetchTickets } from '@/app/lib/data';
+import Loading from './loading';
+import PaginationControls from '@/app/components/pagination-controls';
+import { TicketStatusTabs } from '@/app/components/ticket-status-tabs';
+import Header from '@/app/components/header';
 
-const TicketsList: FC = () => {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined }
+}) {
+  const status = searchParams.status as SupportTicketStatusEnum; 
+  const page = Number(searchParams.page) ?? 1;
+
+  const { data, rowCount } = await fetchTickets(status, page);
+  const totalPages = Math.ceil(rowCount / TICKET_OFFSET);
+
   return (
-    <div className="flex items-center justify-between mt-8">
-      <h1>Ticket List</h1>
+    <div className="bg-white h-3/4 p-8 xl:w-2/3 lg:w-3/4 md:w-full flex flex-col h-screen">
+      <div className="w-full bg-white p-6 rounded-md shadow-md border border-gray-300">
+        <Header header='Ticket List' subheader='View and select existing support tickets.' />
+        <TicketStatusTabs />
+        <Suspense fallback={<Loading />}>
+          <DataList 
+            data={data}
+            DataRow={TicketRow}
+          />
+        </Suspense>
+        <PaginationControls 
+          currentPage={Number(page)} 
+          totalPages={totalPages} 
+        />
+      </div>
     </div>
-  )
+  );
 }
-
-export default TicketsList;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import { CreateTicketForm } from "./createTicketForm";
+"use server";
+import { CreateTicketForm } from "./components/forms/create-ticket-form";
+import Header from "./components/header";
 
-export default function Home() {
+export default async function Page() {
   return (
-    <div className="bg-gray-100 p-8">
-      <div className="max-w-2xl mx-auto bg-white p-6 rounded-md shadow-md">
-        <h1 className="text-xl font-semibold text-gray-700">Create New Ticket</h1>
-        <p className="text-gray-600">Fill out the form and then submit your ticket.</p>
+    <div className="bg-white p-8 xl:w-1/2 lg:w-2/3 md:w-full">
+      <div className="bg-white p-6 rounded-md shadow-md border border-gray-300">
+        <Header header='Create New Ticket' subheader='Fill out the form and then submit your ticket.' />
         <CreateTicketForm />
       </div>
     </div>
